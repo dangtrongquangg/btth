6afad07f6e94e0e9eb291088be10600ff30e015d
dangtrongquangg <115477149+dangtrongquangg@users.noreply.github.com>
2022-12-15 07:56:23 +0700

    Add files via upload

---
+++ 5.4.py
@@ -0,0 +1,6 @@
+import numpy as np
+my_x = np.arange(12, 38)
+print(my_x)
+reverse_x=my_x[::-1]
+print(reverse_x)
+
---
+++ 5.5 (1).py
@@ -0,0 +1,10 @@
+print('danh sach co bao nhieu phan tu n=')
+n=int(input())
+lap=range(0,n,1)
+ds=[]
+for x in lap:
+    print('nhap gia tri phan tu tai dia chi',x,'=')
+    gt=int(input())
+    ds.append(gt)
+ln=ds[0]
+
